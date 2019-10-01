546753fe7c9b47f1e4169a5bd86f0196d1b01d92
ElRaptorus <christian.werner@5minds.de>
2019-10-01 12:32:44 +0200

    :package: Add reinstall script

---
+++ reinstall.sh
@@ -0,0 +1,20 @@
+# remove "node_modules"
+echo "Purging node_modules..."
+find . -name "node_modules" -exec rm -rf '{}' +
+echo "Done. Starting setup..."
+
+# install all necessary dependencies
+npm install --no-package-lock
+
+# If npm install (or minstall) fails, stop any further execution.
+# This is advisable since a failed npm install may lead to failures in the
+# building process.
+if [[ "$?" -ne "0" ]]; then
+  printf "\e[1;31mError while executing npm install!\e[0m\n";
+  exit 1;
+fi
+
+# build all packages
+npm run build
+
+echo "done"
