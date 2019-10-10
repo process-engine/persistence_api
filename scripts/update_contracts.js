9c189c2b3adeff6c2d356d1cc98155b8d8582a24
ElRaptorus <christian.werner@5minds.de>
2019-10-10 11:47:39 +0200

    :recycle: Merge convenience scripts

--- scripts/update_contracts.js
+++ scripts/update_contracts.js
@@ -2,6 +2,12 @@ const exec = require('./exec_async').execAsync;
 const fs = require('fs');
 const path = require('path');
 
+const branchToNpmTagMap = {
+  develop: 'alpha',
+  beta: 'beta',
+  master: 'latest',
+};
+
 console.log(`Command args: ${process.argv.join('\n')}`);
 
 let pushImmediately = false;
@@ -9,28 +15,65 @@ let pushImmediately = false;
 // Note that this script will not work on Jenkins, because Jenkins Builds always checkout a commit, not a branch.
 async function parseCommandLineArgs() {
 
-  if (process.argv.length < 3) {
-    console.error('You must provide a tag name as first argument!');
+  const branchNameArgument = process.argv.find((entry) => entry.startsWith('branch='));
+
+  if (!branchNameArgument) {
+    console.error('You must provide "branch=<master|develop|beta|feature>" as argument!');
     process.exit(1);
   }
 
-  npmTagToUse = process.argv[2];
+  const branchName = branchNameArgument.split('=')[1];
 
-  if (npmTagToUse !== 'alpha' && npmTagToUse !== 'beta' && npmTagToUse !== 'latest') {
-    console.error('Tag name must be either "alpha", "beta" or "latest"!');
+  const branchNameIsInvalid = branchName !== 'develop' && branchName !== 'beta' && branchName !== 'master' && branchName !== 'feature';
+  if (branchNameIsInvalid) {
+    console.error('Value for "branch" must be either "develop", "beta", "master" or "feature"!');
     process.exit(1);
   }
 
-  console.log(`Setting version from npm tag: ${npmTagToUse}`);
+  console.log(`Setting version from ${branchName} branch`);
 
-  pushImmediately = process.argv.find((entry) => entry === 'pushImmediately') !== undefined;
+  pushImmediately = process.argv.find((entry) => entry.startsWith('pushImmediately')) !== undefined;
 
   console.log(`Pushing changes immediately: ${pushImmediately}`);
 
-  return Promise.resolve(npmTagToUse);
+  return Promise.resolve(branchName);
 }
 
-async function getVersionForNpmTag(npmTag) {
+async function getVersionForBranchName(branchName) {
+
+  const useBranchName = branchName === 'feature';
+  if(useBranchName) {
+    console.log(`Setting current feature branch as version.`);
+    return getVersionFromCurrentBranch();
+  }
+
+  const npmTagToUse = branchToNpmTagMap[branchName];
+
+  console.log(`${branchName} is a main branch. Setting version from associated npm tag "${npmTagToUse}".`);
+
+  return getVersionFromNpmTag(npmTagToUse);
+}
+
+async function getVersionFromCurrentBranch() {
+
+  const currentBranch = await exec(`git branch | grep \\* | cut -d ' ' -f2`);
+
+  const sanitizedBranchVersion = currentBranch
+    .replace('/', '~')
+    .replace('\n', '');
+
+  const currentlyOnMainBranch = currentBranch === 'develop' || currentBranch === 'beta' || currentBranch === 'master';
+  if (currentlyOnMainBranch) {
+    console.error('Setting branch versions is not allowed for develop, beta or master!');
+    process.exit(1);
+  }
+
+  console.log(`Setting branch version ${sanitizedBranchVersion} for all layer packages`);
+
+  return sanitizedBranchVersion;
+}
+
+async function getVersionFromNpmTag(npmTag) {
 
   const tagsAsString = await exec('npm dist-tag ls @process-engine/persistence_api.contracts');
 
@@ -80,19 +123,19 @@ async function createCommit() {
   console.log('Commiting everything');
 
   try {
-    await exec('git add */package.json');
-    await exec('git commit -m ":recycle: Set feature branch version for persistence_api.contracts"');
+    // await exec('git add */package.json');
+    // await exec('git commit -m ":recycle: Set feature branch version for persistence_api.contracts"');
 
-    if (pushImmediately) {
-      await exec('git push');
-    }
+    // if (pushImmediately) {
+    //   await exec('git push');
+    // }
   } catch (error) {
     console.log('Failed to create commit. This likely means that the versions are already set and that there is nothing to commit.');
   }
 }
 
 parseCommandLineArgs()
-  .then(getVersionForNpmTag)
+  .then(getVersionForBranchName)
   .then(setContractsVersionInLayerPackages)
   .then(createCommit)
   .then(() => console.log('Done!'))
